96ee7bf53cfe86c21b8d938c8c3ba64b852895ee
HeQiang <heqiangrd@gmail.com>
2017-07-05 01:22:19 +0800

    IteratorAggregate.php

---
+++ 3.聚合式迭代器/IteratorAggregate.php
@@ -0,0 +1,21 @@
+<?php
+
+class myData implements IteratorAggregate {
+
+    public $str = 'one';
+    public $arr = array('a','b','c','d');
+
+    /**
+     * 获取一个外部迭代器
+     * @return ArrayIterator 返回一个外部迭代器。
+     */
+    public function getIterator() {
+        return new ArrayIterator($this);
+    }
+
+}
+
+$obj = new myData(array('1','2'));
+foreach ($obj as $k=>$v) {
+    var_dump($k,$v,'——————');
+}
